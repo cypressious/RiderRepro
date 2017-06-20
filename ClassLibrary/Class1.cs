21e8b6ba01bc77112187b484859267c54a0f0332
Kirill Rakhman <kirill.rakhman@gmail.com>
2017-06-20 14:25:49 +0200

    inital commit

---
+++ ClassLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿
+namespace ClassLibrary
+{
+    public class Class1
+    {
+        public string Bar { get; set; } = "Bar2";
+    }
+}
---
+++ WebApplication/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [Route("api")]
+    public class ValuesController : Controller
+    {
+        private readonly Class1 foo;
+
+        public ValuesController(Class1 foo)
+        {
+            this.foo = foo;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Foo2 " + foo.Bar;
+        }
+    }
+}
